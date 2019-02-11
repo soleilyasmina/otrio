92079b5a54797153a8e0fd1302468b7d76a287e1
soleilyasmina <mounlasolomon@gmail.com>
2019-02-11 12:57:21 -0500

    added space

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React, { Component } from 'react';
 import './App.css';
+import Space from './components/Space';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
+        <Space />
       </div>
     );
   }
