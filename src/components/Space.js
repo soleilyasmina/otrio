9810fc5792b9b62ea95faea26574862afdf68da8
soleilyasmina <mounlasolomon@gmail.com>
2019-02-11 15:43:45 -0500

    added props

--- src/components/Space.js
+++ src/components/Space.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import '../App.css';
 import Piece from './Piece';
+
 export default function Space(props) {
   return (
     <div className="space">
-      <Piece size="large" color="red" />
-      <Piece size="medium" color="red" />
-      <Piece size="small" color="red" />
+      <Piece size="large" color={props.space.large} />
+      <Piece size="medium" color={props.space.medium} />
+      <Piece size="small" color={props.space.small} />
     </div>
   )
 }
