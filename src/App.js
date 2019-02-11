7a25b11da8398c145ba4ba86805d43ed603885e6
soleilyasmina <mounlasolomon@gmail.com>
2019-02-11 16:23:10 -0500

    added player component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React, { Component } from 'react';
 import './App.css';
 import Board from './components/Board';
+import Player from './components/Player';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <Board />
+        <Player />
       </div>
     );
   }
