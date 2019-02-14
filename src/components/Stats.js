5a972f3cf50e0458aa20e8a922f8d1182ea2174c
soleilyasmina <mounlasolomon@gmail.com>
2019-02-14 13:44:01 -0500

    added css and an enclosing div

--- src/components/Stats.js
+++ src/components/Stats.js
@@ -2,6 +2,8 @@ import React from 'react';
 
 export default function Stats(props) {
   return (
-    <h1>Player {props.turn + 1}{props.win ? ' wins!' : "'s turn"}</h1>
+    <div className="">
+      <h1>Player {props.turn + 1}{props.win ? ' wins!' : "'s turn"}</h1>
+    </div>
   )
 }
