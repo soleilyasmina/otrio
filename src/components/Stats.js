df7e49bccd7478bd818c28c87437764f89ae79cb
soleilyasmina <mounlasolomon@gmail.com>
2019-02-25 13:37:51 -0500

    added more info and usernames to stats

--- src/components/Stats.js
+++ src/components/Stats.js
@@ -2,8 +2,18 @@ import React from 'react';
 
 export default function Stats(props) {
   return (
-    <div className="">
-      <h1>Player {props.turn + 1}{props.win ? ' wins!' : "'s turn"}</h1>
+    <div className="stats">
+      <h1>{props.scores[props.turn].username}{props.win ? ' wins!' : "'s turn"}</h1>
+      <div className="points-container">
+        {props.scores.map(score => {
+          return (
+            <div className="score-container">
+              <p>{score.points}</p>
+              <h4>{score.username}</h4>
+            </div>
+          )
+        })}
+      </div>
     </div>
   )
 }
