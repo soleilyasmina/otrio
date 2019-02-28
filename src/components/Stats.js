06d9d2fa85b2003a54d1ec8bcce3467e0e0340ef
soleilyasmina <mounlasolomon@gmail.com>
2019-02-28 12:16:40 -0500

    reorganized title and points

--- src/components/Stats.js
+++ src/components/Stats.js
@@ -3,13 +3,14 @@ import React from 'react';
 export default function Stats(props) {
   return (
     <div className="stats">
-      <h1>{props.scores[props.turn].username}{props.win ? ' wins!' : "'s turn"}</h1>
+      <h1>OTRIO</h1>
+      <h3>{props.scores[props.turn].username}{props.win ? ' wins!' : "'s turn"}</h3>
       <div className="points-container">
         {props.scores.map(score => {
           return (
             <div className="score-container">
-              <p>{score.points}</p>
               <h4>{score.username}</h4>
+              <h4>{score.points}</h4>
             </div>
           )
         })}
